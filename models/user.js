3576eaf35f9409fc431845cf8e883150dbb5b944
Dongjun Yu <200372591@student.georgianc.on.ca>
2019-07-12 11:43:02 -0400

    Add Navigation Bar

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const findorcreate = require('mongoose-findorcreate');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  username: String,
+  password: String
+});
+
+userSchema.plugin(findorcreate);
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
