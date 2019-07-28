23d7dd80f4c8ddf87be5baddd5d5b96753f5c75b
Tomoya Kuroda <31934952+tomoyakuroda@users.noreply.github.com>
2019-07-28 00:59:53 -0400

    Revert "Create survey"

--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,20 @@ var router = express.Router();
 
 
 
-// get  surveys for registered account
-router.get('/:username/surveys', function(req, res, next) {
-  res.render('users/surveys',  surveys.findAllSurveys)
+// get users surveys and other suryes
+router.get('/surveys', function(req, res, next) {
+  res.render('users/surveys',  { username: req.user.username })
 });
 
 
 router.post('/:username/create', surveys.createNewSurvey);
 
 /* GET new survey page */
-router.get('/:username/create',  function(req, res, next) {
-  if(!req.user){
-    res.redirect('/')
-  }else if(req.params.username == req.user.username){
+router.get('/:username/create', function(req, res, next) {
   res.render('users/create')
-  }else 
-  res.redirect('/')
 });
 
 
-
-
-
 router.get('/:username/surveys', surveys.findAllSurveys);
-
 
 module.exports = router;
