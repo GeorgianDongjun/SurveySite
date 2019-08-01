99e4f819ff7bd710c4f16cd4582a4567024906d0
yangjack1989 <200362300@student.georgianc.on.ca>
2019-08-01 01:14:25 -0400

    fix surveys link in header

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var surveys = require('./controllers/surveys');
+const Survey=require('../models/surveys');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 // List all surveys(GET)
 router.get('/surveys', surveys.findAllSurveys);
+
+/* // List all surveys(GET)
+router.get('/surveys', async function(req,res,next){
+  const surveys = await Survey.find();
+  console.log(surveys)
+  res.render('users/surveys',surveys)
+}); */
 
 // List a specific surveys (GET)
 
